c6231e711fbabcc60f3369756b96cd545373b9a6
Victor Radael <victorradael@hotmail.com>
2020-08-29 18:14:44 -0300

    :rocket: Att CI

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Eu sou Full Cycle!';
+    return 'Eu sou Full Cycle! CI NELES';
   }
 }
